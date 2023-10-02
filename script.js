4e3164c37ad7edd97f6244bf72e655f9cbc8f909
ispadilha <israel.padilha@gmail.com>
2023-10-02 06:56:36 -0300

    Refac from .then .catch .finally to  async await

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro){
-            throw Error('Esse CEP não existe!')
-        } else {
-            console.log(r)}
+async function buscaEndereco(cep){
+    try{
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json()
+        if(consultaCEPConvertida.erro){
+            throw Error('CEP inexistente!')
         }
-     )
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído'))
+        console.log(consultaCEPConvertida)
+        return consultaCEPConvertida
+    } catch(erro){
+        console.log(erro)
+    }
+}
 
-console.log(consultaCEP)
+let ceps = ['01001000','01001001']
+let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores))
+console.log(conjuntoCeps)
+Promise.all(conjuntoCeps).then((respostas) => console.log(respostas))
