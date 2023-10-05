6fc597ff2cab5522d82461e772c27c53bab9da92
ispadilha <israel.padilha@gmail.com>
2023-10-05 08:24:36 -0300

    Added autocompletion

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = ""
+
     try{
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json()
         if(consultaCEPConvertida.erro){
             throw Error('CEP inexistente!')
         }
+
+        var cidade = document.getElementById('cidade')
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+
+        cidade.value = consultaCEPConvertida.localidade
+        logradouro.value = consultaCEPConvertida.logradouro
+        estado.value = consultaCEPConvertida.uf
+
         console.log(consultaCEPConvertida)
         return consultaCEPConvertida
+
     } catch(erro){
+        mensagemErro.innerHTML =
+            `<p style="
+                margin-top: 15px;
+                color: red;
+            ">
+                CEP inválido. Tente novamente!
+            </p>`
         console.log(erro)
     }
 }
@@ -16,3 +36,6 @@ let ceps = ['01001000','01001001']
 let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores))
 console.log(conjuntoCeps)
 Promise.all(conjuntoCeps).then((respostas) => console.log(respostas))
+
+var cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
